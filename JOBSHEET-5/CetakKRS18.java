12940bfc29cd88110b235a24c6b4daf3112c8acb
m-zasky <mmuhammadzasky@gmail.com>
2025-10-08 08:49:26 +0700

    Percobaan1

---
+++ JOBSHEET-5/CetakKRS18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CetakKRS18 {
+    public static void main(String[] args) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("pembayaran UKT terverivikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+        
+    }
+
+}
